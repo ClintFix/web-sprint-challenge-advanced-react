32e092e4a84473fe85ddc263d933cca288db74d7
ClintFix <clint.fix@gmail.com>
2021-03-12 08:52:57 -0700

    form header renders test complete

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,15 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    //Arrange: render component
+    render(<CheckoutForm/>)
+    //Act: find the header on the page
+    const header = screen.getByText(/checkout form/i)
+    //assert: text that it is on the document (using getByText in act, so will throw an error if not there)
+});
 
 test("form shows success message on submit with form details", () => {});
